e87c3b75c546e88e0a65f2e0703afc37c75b7d84
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-09-06 13:41:32 -0700

    line too long pppfffttt

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -6,11 +6,13 @@ number = random.randint(-10000, 10000)
 last_digit = abs(number) % 10
 last_digit *= -1 if number < 0 else 1  # Preserve the sign of the original number
 
-print(f"Last digit of {number} is {last_digit}", end=" ")
+output = f"Last digit of {number} is {last_digit}"
 
 if last_digit > 5:
-    print("and is greater than 5")
+    output += " and is greater than 5"
 elif last_digit == 0:
-    print("and is 0")
+    output += " and is 0"
 else:
-    print("and is less than 6 and not 0")
+    output += " and is less than 6 and not 0"
+
+print(output)
