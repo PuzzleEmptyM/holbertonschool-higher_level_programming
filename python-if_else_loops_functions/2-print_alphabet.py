adb1c3c6f0d77483fd6d82b64a8af5dca40ce60f
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-09-06 13:45:13 -0700

    here we gp

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 for char in range(ord('a'), ord('z') + 1):
-    print(chr(char), end='')
+    print("{:c}".format(char), end='')
 
 # Print a newline character to separate the output from the shell prompt
 print()
