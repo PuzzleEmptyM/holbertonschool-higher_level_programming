9c510586dd2ca278b09456b8f966f62484225757
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-09-06 13:47:04 -0700

    updated #2

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
-for char in range(ord('a'), ord('z') + 1):
-    print("{:c}".format(char), end='')
+for char_code in range(97, 123):
+    print("{:c}".format(char_code), end="")
 
-# Print a newline character to separate the output from the shell prompt
-print()
+print()  # Print newline character to separate output from the shell
