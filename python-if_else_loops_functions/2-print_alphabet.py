05c94083e5001c2c547cf914377c9465dfcb7203
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-09-06 13:50:11 -0700

    here we go

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 
 for char_code in range(ord('a'), ord('z') + 1):
-    print(f"{chr(char_code)}", end="")
+    print("{}".format(chr(char_code)), end="")
+
+# Print a newline character to separate the output from the shell prompt
+print()
