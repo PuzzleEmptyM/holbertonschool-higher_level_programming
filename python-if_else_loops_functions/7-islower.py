b706e1cd4920f1bc3567eb650846f1ccc22f97fd
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-09-06 22:33:59 -0700

    Write a function that checks for lowercase character.

---
+++ python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,12 @@
+def islower(c):
+    # Check if the ASCII value of the character falls within the lowercase range
+    return ord('a') <= ord(c) <= ord('z')
+
+# Test cases
+if __name__ == "__main__":
+    print("a is {}".format("lower" if islower("a") else "upper"))
+    print("H is {}".format("lower" if islower("H") else "upper"))
+    print("A is {}".format("lower" if islower("A") else "upper"))
+    print("3 is {}".format("lower" if islower("3") else "upper"))
+    print("g is {}".format("lower" if islower("g") else "upper"))
+
