1a6bc57ef46ce22d1cf767656b5dcb12f5efc40f
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-09-12 16:11:29 -0700

    added newline argument

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
-for c in range(ord('z'), ord('a') - 1, -1):
-    print("{:c}".format(c if c % 2 == 0 else c - 32), end="")
+for c in range(ord('z'), ord('A') - 1, -1):
+    if c % 2 == 0:
+        print("{:c}".format(c), end="")
+    else:
+        print("{:c}".format(c - 32), end="")
 print()
