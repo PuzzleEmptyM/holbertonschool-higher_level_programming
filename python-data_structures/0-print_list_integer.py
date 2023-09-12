d595cfaf3d4846217afd64e80ec3459a19ebc43d
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-09-12 11:08:09 -0700

    removed main file and fixed file 0

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -4,6 +4,7 @@ def print_list_integer(my_list=[]):
     for num in my_list:
         print("{}".format(num))
 
+
 if __name__ == "__main__":
     my_list = [1, 2, 3, 4, 5]
     print_list_integer(my_list)
