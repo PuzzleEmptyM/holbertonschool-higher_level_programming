f02f7ff9ef34f958825968fa646947caaa2cb514
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-09-12 12:56:29 -0700

    removed contents of 6-main from 6-print

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -5,10 +5,3 @@ def print_matrix_integer(matrix=[[]]):
         for num in row:
             print("{:d}".format(num), end=" ")
         print()
-
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
