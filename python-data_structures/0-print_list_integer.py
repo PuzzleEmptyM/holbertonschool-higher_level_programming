1e85ed80c40ba1ff0ff5763993a87f2f6301a075
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-09-12 11:11:58 -0700

    lets see if this works

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
+# 0-print_list_integer.py
 
 def print_list_integer(my_list=[]):
-    for num in my_list:
-        print("{}".format(num))
-
-
-if __name__ == "__main__":
-    my_list = [1, 2, 3, 4, 5]
-    print_list_integer(my_list)
+    """Print all integers of a list."""
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
