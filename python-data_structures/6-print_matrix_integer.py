0162afa85d5bf0f950f70c7ce0c46705ff35de85
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-09-12 12:51:00 -0700

    Write a function that prints a matrix of integers.

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -5,3 +5,13 @@ def print_matrix_integer(matrix=[[]]):
         for num in row:
             print("{:d}".format(num), end=" ")
         print()
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print_matrix_integer(matrix)
+print("--")
+print_matrix_integer()
