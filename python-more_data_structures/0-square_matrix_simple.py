7c77505637e569bf205818ab3f0ade6d1526d4d0
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-09-12 13:42:37 -0700

    switched to my original code

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    # Compute the square value of all integers of a matrix.
-    return ([list(map(lambda x: x * x, row)) for row in matrix])
+    new_matrix = []
+
+    for row in matrix:
+        new_row = []
+        for num in row:
+            new_row.append(num ** 2)
+        new_matrix.append(new_row)
+
+    return new_matrix
