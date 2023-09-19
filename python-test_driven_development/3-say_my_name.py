0e58727f1c621b89890d74a493216f311971e07f
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-09-19 16:30:54 -0700

    added documentation

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""Prints My first name is <first name> <last name>"""
 
 def say_my_name(first_name, last_name=""):
+    """Prints My name is <first name> <last name>"""
+
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
