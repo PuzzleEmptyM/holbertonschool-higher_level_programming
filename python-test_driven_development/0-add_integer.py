0b7e606837af56ef3eef97f5f6032210488292be
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-09-19 14:38:14 -0700

    finally passed doctest on task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
+"""Function that adds two integers"""
+
 
 def add_integer(a, b=98):
-    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+    """
+    Function that adds two integers and returns their
+    sum as an integer
+
+    Raises:
+    TypeError: if 'a' or 'b' is not a number
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    return int(a) + int(b)
+    return int(round(a + b))
