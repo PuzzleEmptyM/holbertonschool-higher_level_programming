ff6cb1ad9e8dfe395897a9e843df062b4a657375
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-09-19 20:58:29 -0700

    hopefully fixed an output check on the checker

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -47,3 +47,13 @@ class Rectangle:
     def perimeter(self):
         """Calculate the perimeter of the rectangle."""
         return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """Return a string representation of the rectangle."""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        return "\n".join(["#" * self.__width] * self.__height)
+
+    def __repr__(self):
+        """Return a string representation of the rectangle."""
+        return f"Rectangle({self.__width}, {self.__height})"
