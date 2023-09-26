9f9401ff9bb94dd324a040b743cc1a3ad5b81441
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-09-26 11:42:38 -0700

    square subclass

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" introduce Square subclass of Rectangle """
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Square subclass of Rectangle """
+    pass
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
