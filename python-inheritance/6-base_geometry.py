bbb746d88eb763951a22b51b6de6a1851b8b4ace
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-09-26 11:22:18 -0700

    method that raises exception

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" introduce area method that raises exception """
+
+
+class BaseGeometry:
+    """ introduce area method that raises exception """
+    def area(self):
+        raise Exception("area() is not implemented")
