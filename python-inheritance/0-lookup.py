e798ad76f8276e98c8df9f641d54de2835300b64
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-09-26 11:04:46 -0700

    test push

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" returns list of object attributes & methods """
+
+
+def lookup(obj):
+    """ returns list of object attributes & methods """
+    return dir(obj)
---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" MyList class inherited from list - sorts input """
+
+
+class MyList(list):
+    """ MyList class inherited from list - sorts input """
+    def print_sorted(self):
+        """ prints sorted list """
+        print(sorted(self))
