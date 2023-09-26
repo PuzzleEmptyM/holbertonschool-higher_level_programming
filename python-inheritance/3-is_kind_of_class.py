a2b712f7959df11a2653f089c25fa1aa1d284b12
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-09-26 11:16:50 -0700

    test if the object is an instance of the specified class

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" verifies if input object is inheritance instance of input class """
+
+
+def is_kind_of_class(obj, a_class):
+    """ verifies if input object is inheritance instance of input class """
+    return isinstance(obj, a_class)
