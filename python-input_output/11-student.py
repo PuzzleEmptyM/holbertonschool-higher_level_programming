239d079af20580dce80f8897a3c3739a01e3d099
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-09-26 20:41:12 -0700

    Student to disk and reload

---
+++ python-input_output/11-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" module containing definition of Student class """
+
+
+class Student:
+    """ Student class containing first_name, last_name, & age """
+    pass
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if type(attrs) is list and all(type(entry) is str for entry in attrs):
+            attr_dict = {}
+            for key in attrs:
+                if hasattr(self, key):
+                    attr_dict[key] = getattr(self, key)
+            return attr_dict
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        return self.__dict__.update(json)
