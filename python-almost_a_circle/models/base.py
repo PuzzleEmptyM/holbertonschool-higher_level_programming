45772ae6acab3d4f0e1b579a58f77ff0348ac282
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-10-02 13:54:59 -0700

    Fixed pycode errors

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
+""" Base.py Module """
 import json
 from os.path import isfile
+
 
 class Base:
     """ Base class initiated """
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """ If id is provided, assign it to the public instance attribute id """
+        """ If id is provided, assign to public instance attribute id """
         if id is not None:
             self.id = id
         else:
-            """ Otherwise, increment __nb_objects and assign the new value to id """
+            """ Otherwise increment __nb_objects and assign new value to id """
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
