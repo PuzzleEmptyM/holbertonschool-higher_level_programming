6269afaa9315688ce832d733b69226232e56e6ea
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-10-02 14:52:13 -0700

    Updated the class Rectangle by adding the public method def update(self, *args):

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -88,3 +88,16 @@ class Rectangle(Base):
         """ Override the string representation of Rectangle instances """
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
             self.id, self.__x, self.__y, self.__width, self.__height)
+
+    def update(self, *args):
+        """ Method to update attributes based on no-keyword arguments"""
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.width = args[1]
+        if len(args) >= 3:
+            self.height = args[2]
+        if len(args) >= 4:
+            self.x = args[3]
+        if len(args) >= 5:
+            self.y = args[4]
