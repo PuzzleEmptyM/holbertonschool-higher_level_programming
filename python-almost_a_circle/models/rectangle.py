bd07b3393b32b34a0f6a21f3b3add2fb33db30ef
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-10-02 18:12:33 -0700

    fixed the order of the things in the dictionary

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -118,5 +118,5 @@ class Rectangle(Base):
 
     def to_dictionary(self):
         """ Return dictionary representation of Rectangle """
-        return {'x': self.x, 'y': self.y, 'id': self.id,
-                'height': self.height, 'width': self.width}
+        return {'id': self.id, 'x': self.x, 'size': self.size,
+                'y': self.y}
