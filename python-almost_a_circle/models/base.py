47295b3a018a371c745bc38b47d9d0ef0c6bd84b
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-10-02 18:20:56 -0700

    made quite a few changes and forgot to push

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -16,3 +16,10 @@ class Base:
             """ Otherwise increment __nb_objects and assign new value to id """
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ Convert a list of dictionaries to a JSON string """
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
