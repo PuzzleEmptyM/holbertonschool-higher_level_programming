27b8fe1a0d093934e969787316421d14ebef3cd8
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-10-24 23:26:13 -0500

    changed syntax drastically

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,38 +1,20 @@
 #!/usr/bin/python3
+""" lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
-import sys
+from sys import argv
+
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
-
-    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
-
-    # Connect to the MySQL server
     db = MySQLdb.connect(
-        host="localhost",
+        host='localhost',
         port=3306,
-        user=username,
-        passwd=password,
-        db=database,
-        charset="utf8"
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
     )
-
-    # Create a cursor object to interact with the database
     cur = db.cursor()
-
-    # Execute the SQL query to retrieve the states
-    query = "SELECT * FROM states ORDER BY id ASC"
-    cur.execute(query)
-
-    # Fetch all the results
-    states = cur.fetchall()
-
-    # Display the results
-    for state in states:
-        print(state)
-
-    # Close the cursor and the database connection
+    cur.execute("SELECT * FROM states")
+    output = cur.fetchall()
+    [print(item) for item in output]
     cur.close()
     db.close()
