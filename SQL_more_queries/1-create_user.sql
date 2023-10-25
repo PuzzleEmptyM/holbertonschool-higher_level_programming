82c0b4a8ab0da791a8672c29be573303a4303eed
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-10-24 22:30:54 -0500

    -- Create or update user user_0d_1

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- Create or update user user_0d_1
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges to user_0d_1
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
