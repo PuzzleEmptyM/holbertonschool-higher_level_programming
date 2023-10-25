28a59ab656a1a12601a5f9604409d01bcc7139c2
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-10-24 23:16:12 -0500

    adjusted syntax

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,11 +1,8 @@
 -- List all shows and linked genres
-SELECT
-    tv_shows.title,
-    IFNULL(GROUP_CONCAT(tv_genres.name ORDER BY tv_genres.name ASC SEPARATOR ', '), 'NULL') AS genre
+SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
 LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id
-GROUP BY tv_shows.title
-ORDER BY tv_shows.title, genre;
+ORDER BY tv_shows.title, tv_genres.name ASC;
