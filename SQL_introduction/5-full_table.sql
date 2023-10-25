2c6865e910fb491f8bd294a6cc2a761dd07eb097
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-10-25 13:38:41 -0500

    rewrite

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,4 +1,2 @@
 -- Print the full description of the table
-SELECT *
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+SHOW CREATE TABLE first_table
