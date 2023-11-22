53b8c240361cf3423e07ae66d85f62c5215695d4
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-11-22 14:49:35 -0600

    Write a script that prints two arguments passed to it, in the following format: “ is ”

---
+++ javascript-warm_up/4-concat.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+// Get number of arguments
+const firstArg = process.argv[2];
+const secondArg = process.argv[3];
+
+// Use console.log(...) to print the output based on args
+console.log(`${firstArg} is ${secondArg}`);
