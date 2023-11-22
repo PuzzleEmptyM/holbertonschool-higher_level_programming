dba153abebb5ce3bf90c0e48a543fb8cfcc6a706
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-11-22 14:40:27 -0600

    removed last else credentials

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -6,8 +6,10 @@ const numArgs = process.argv.length - 2;
 // Use console.log(...) to print the output based on args
 if (numArgs === 0) {
     console.log('No argument');
-} else if (numArgs === 1) {
+} 
+else if (numArgs === 1) {
     console.log('Argument found');
-} else (numArgs === 2) {
+} 
+else {
     console.log('Arguments found');
 }
