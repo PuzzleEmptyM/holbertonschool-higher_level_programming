763a73eb4cc52bbe84f54c0e33a377d9f97c9e98
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-11-22 14:29:39 -0600

    mulitple print lines

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+// Create a constant variable called myVar
+const myVar1 = 'C is fun';
+const myVar2 = 'Python is cool';
+const myVar3 = 'JavaScript is amazing';
+
+// Use console.log(...) to print the output
+console.log(myVar1, myVar2, myVar3);
