eb17ccfadce8e31847ca3ced326ef80e6ed89899
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-11-22 15:00:10 -0600

    Cool to cool

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 // Get number of arguments
-const languages = ['C is fun', 'Python is Cool', 'JavaScript is amazing'];
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 // Use console.log(...) to print the output based on args
 for (let i = 0; i < languages.length; i++) {
