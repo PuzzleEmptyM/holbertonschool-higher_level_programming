ffdad0cc66f11582e7902fd68089e3b11cb50bad
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-11-22 14:41:55 -0600

    semistandard

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -5,11 +5,9 @@ const numArgs = process.argv.length - 2;
 
 // Use console.log(...) to print the output based on args
 if (numArgs === 0) {
-    console.log('No argument');
-} 
-else if (numArgs === 1) {
-    console.log('Argument found');
-} 
-else {
-    console.log('Arguments found');
+  console.log('No argument');
+} else if (numArgs === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
