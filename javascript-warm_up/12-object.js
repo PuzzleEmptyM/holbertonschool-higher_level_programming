a2a3c662f5b7d9200434855356f36714328cced7
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-11-27 16:49:30 -0600

    script initializes an object myObject with a type and a value property, and then updates the value property from 12 to 89

---
+++ javascript-warm_up/12-object.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const myObject = {
+  type: 'object',
+  value: 12
+};
+
+console.log(myObject);
+
+// Update the value from 12 to 89
+myObject.value = 89;
+
+console.log(myObject);
