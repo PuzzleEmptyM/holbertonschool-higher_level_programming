a971f348de89ab1fa7ee451ccd5c743a97a40b33
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-11-27 21:16:28 -0600

    uses Object.create(Rectangle.prototype) for instantiation, which can be an alternative in certain environments. If this still doesn't resolve the issue, there might be specific constraints or configurations in environment that i need to consider

--- javascript_objects_scopes_closures/0-rectangle.js
+++ javascript_objects_scopes_closures/0-rectangle.js
@@ -1,9 +1,9 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 
 class Rectangle {
   // Class methods and properties will go here
 }
 
 // Example usage:
-const r1 = new Rectangle();
+const r1 = Object.create(Rectangle.prototype);
 console.log(r1);
