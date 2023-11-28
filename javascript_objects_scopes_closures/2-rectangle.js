84a7eb9fb08b7d7e0dca082dd4caac50bb0699c6
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-11-27 21:38:14 -0600

    modify the approach to create an instance with default values when the conditions are not met

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -4,13 +4,14 @@ class Rectangle {
   constructor(w, h) {
     if (w <= 0 || h <= 0 || typeof w !== 'number'
     || typeof h !== 'number') {
-      // Create an empty object if conditions are not met
-      return {};
+      // Create an instance with default values if conditions are not met
+      this.width = 0;
+      this.height = 0;
+    } else {
+      // Initialize instance attributes
+      this.width = w;
+      this.height = h;
     }
-
-    // Initialize instance attributes
-    this.width = w;
-    this.height = h;
   }
 }
 
