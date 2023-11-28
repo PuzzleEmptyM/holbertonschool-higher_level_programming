1a94353fd559e78308e435c38421fcf2b2bf46bf
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-11-27 21:21:28 -0600

    This class has a constructor that takes two arguments w and h and initializes the instance attributes width and height with the values of w and h, respectively.

---
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor(w, h) {
+    // Initialize instance attributes
+    this.width = w;
+    this.height = h;
+  }
+}
+
+// Export the Rectangle class
+module.exports = Rectangle;
