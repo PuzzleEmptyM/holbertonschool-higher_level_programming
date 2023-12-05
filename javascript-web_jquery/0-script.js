f804bc08c55a29ae4f61b90d58191e703ae56c48
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-12-05 22:15:59 +0000

    removed first line (redundant)

--- javascript-web_jquery/0-script.js
+++ javascript-web_jquery/0-script.js
@@ -1,25 +1,15 @@
-/* global $ */
-
 document.addEventListener('DOMContentLoaded', function () {
+  // Wait for the DOM content to be fully loaded
 
-    // Wait for the DOM content to be fully loaded
+  const header = document.querySelector('header');
 
-    var header = document.querySelector('header');
+  // Check if the <header> element is found
 
+  if (header) {
+    // Update the text color to red (#FF0000)
 
-
-    // Check if the <header> element is found
-
-    if (header) {
-
-        // Update the text color to red (#FF0000)
-
-        header.style.color = '#FF0000';
-
-    } else {
-
-        console.error('Error: <header> element not found');
-
-    }
-
+    header.style.color = '#FF0000';
+  } else {
+    console.error('Error: <header> element not found');
+  }
 });
