01a31e75239ea264681096df3f417bfbc9b14453
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-12-06 00:00:47 -0600

    overthought once again

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,20 +1,4 @@
 #!/usr/bin/node
-const request = require('request');
 const fs = require('fs');
-
-const url = process.argv[2];
-const filePath = process.argv[3];
-
-request(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else {
-    fs.writeFile(filePath, body, 'utf8', (err) => {
-      if (err) {
-        console.error(err);
-      } else {
-        console.log(`${filePath}`);
-      }
-    });
-  }
-});
+const request = require('request');
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
