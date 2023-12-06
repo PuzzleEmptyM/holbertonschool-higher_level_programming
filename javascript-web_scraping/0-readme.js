d9c5ff9302747564fd3c812a8456904f629ef716
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-12-05 23:15:22 -0600

    simplify logic

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,12 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
-
-const filePath = process.argv[2];
-
-fs.readFile(filePath, 'utf-8', (err, data) => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log(data);
-  }
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });
