0dda7df780c449dd7047988346c7a26c51020cce
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-12-06 00:04:55 -0600

    rethought logic

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,19 +1,13 @@
 #!/usr/bin/node
 const request = require('request');
 
-const apiUrl = process.argv[2];
-
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else {
-    const filmsData = JSON.parse(body).results;
-    const wedgeAntillesCount = filmsData.reduce((count, film) => {
-      if (film.characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
-        return count + 1;
-      }
-      return count;
-    }, 0);
-    console.log(wedgeAntillesCount);
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const theAnswer = JSON.parse(body).results;
+    console.log(theAnswer.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
   }
 });
